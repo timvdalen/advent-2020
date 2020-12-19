32b6922e7d37cfbc55e5becc1b9d5ca2c9213732
Tim van Dalen <Tim@timvdalen.nl>
2020-12-19 19:22:00 +0100

    Implement input caching

--- src/helpers.php
+++ src/helpers.php
@@ -12,11 +12,18 @@ if(!function_exists('get_input')) {
 	 * @throws GuzzleException
 	 */
 	function get_input($day) {
+		$cache_key = __DIR__ . "/../cache/$day";
+		if (file_exists($cache_key)) {
+			return file_get_contents($cache_key);
+		}
+
 		$jar = CookieJar::fromArray(['session' => env('AOC_COOKIE')],'.adventofcode.com');
 		$client = new Client();
 		$resp = $client->request('GET', "https://adventofcode.com/2020/day/$day/input", [
 			'cookies' => $jar
 		]);
-		return (string)$resp->getBody();
+		$input = (string)$resp->getBody();
+		file_put_contents($cache_key, $input);
+		return $input;
 	}
 }
